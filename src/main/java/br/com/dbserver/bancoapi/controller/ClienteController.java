0ac99543eeeb115e15b67d0f5009e8c61dbc9ab6
Ludson Brandao <ludsonssb@gmail.com>
2020-03-02 16:52:15 -0300

    alterações no controller

--- src/main/java/br/com/dbserver/bancoapi/controller/ClienteController.java
+++ src/main/java/br/com/dbserver/bancoapi/controller/ClienteController.java
@@ -5,6 +5,8 @@ import br.com.dbserver.bancoapi.model.Cliente;
 import br.com.dbserver.bancoapi.repository.ClienteRepository;
 import br.com.dbserver.bancoapi.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -21,22 +23,44 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
+//    @GetMapping("{id}")
+//    public ClienteResponse buscarPorId(@PathVariable Long id) {
+//
+//        return clienteService.buscarClientePeloCodigo(id);
+//    }
+
     @GetMapping("{id}")
-    public ClienteResponse buscarPorId(@PathVariable Long id) {
-        return clienteService.buscarClientePeloCodigo(id);
+    public ResponseEntity<ClienteResponse> buscarPorId(@PathVariable Long id) {
+        ClienteResponse cliente = clienteService.buscarClientePeloCodigo(id);
+        return cliente != null ? ResponseEntity.ok(cliente) : ResponseEntity.notFound().build();
     }
+
+
+//    @PostMapping
+//    public ClienteResponse cadastrar(@RequestBody ClienteRequest novoCliente) {
+//        return clienteService.cadastroCliente(novoCliente);
+//    }
 
     @PostMapping
-    public ClienteResponse cadastrar(@RequestBody ClienteRequest novoCliente) {
-        return clienteService.cadastroCliente(novoCliente);
+    public ResponseEntity<ClienteResponse> cadastrar(@RequestBody ClienteRequest novoCliente) {
+        ClienteResponse cliente = clienteService.cadastroCliente(novoCliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
     }
 
+//    @PutMapping("{cpf}")
+//    public Optional<Cliente> alterar(@PathVariable String cpf, @RequestBody AlteraClienteDTO alteraCliente){
+//       return clienteService.atualizaClientePorCpf(cpf, alteraCliente);
+//    }
+
     @PutMapping("{cpf}")
-    public Optional<Cliente> alterar(@PathVariable String cpf, @RequestBody AlteraClienteDTO alteraCliente){
-       return clienteService.atualizaClientePorCpf(cpf, alteraCliente);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public  ResponseEntity<Cliente> alterar(@PathVariable String cpf, @RequestBody AlteraClienteDTO alteraCliente){
+        Optional<Cliente> cliente = clienteService.atualizaClientePorCpf(cpf, alteraCliente);
+        return ResponseEntity.ok(cliente.get());
     }
 
     @Transactional
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("{cpf}")
     public void excluirPorCpf(@PathVariable String cpf){
         clienteRepository.deleteByCpf(cpf);
